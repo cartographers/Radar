654a5984023c0d57f547aa85f2aa2a8d5cfa3856
Sulamita <sulamita@Users-MacBook-Pro.local>
2017-08-14 03:46:10 -0400

    chore: I do what

--- client/components/myForm.js
+++ client/components/myForm.js
@@ -10,42 +10,86 @@ class myForm extends React.Component {
     super()
     this.state = {
       selectThese: [{
-        col: 'Choose One'
+        col: 'All'
       }],
-      whereThese: [],
-      orderedBy: 'None'
+      whereThese: [{
+        col: 'none'
+      }],
+      orderedBy: 'None',
+      conditionals : ['greater than', 'greater than or equal to', 'less than', 'less than or equal to','equal to', 'not', 'between', 'not between'],
+      orderType : ['None','Ascending', 'Descending']
     }
-    addSelect = () => ({
-    })
 
-    updateSelect = (index) => ({
-    })
   }
 
   componentDidMount() {
     this.props.fetchAllUsers();
   }
 
+  handleSelectChange = (evt) => {
+    console.log(evt.target.name, evt.target.value)
+    this.setState( (prevState) => ({ selectThese: prevState.map( (val, index) => {
+      return (index === evt.target.name) ? { col: evt.target.value } : val
+    })}))
+  }
+
+  addSelect = (evt) => {
+    console.log('Updating state')
+    this.setState( (prevState) => ({ selectThese: [...prevState.selectThese, {col:'All'}] }))
+    console.log(this.state)
+  }
 
   render () {
-    const { table, columns } = this.props;
+    const { table, columns } = this.props
+    const { conditionals, orderType } = this.state
+
     return (
       <div>
-        <h2>Query Selection Form</h2>
+        <h2>User Query Selection Form</h2>
         <form>
           <div className="form-group">
             <label>Select</label>
             {
-              this.state.selectThese.map((val, index) => ({
-                return  <select className="form-control" name="testSelect">
-                          {columns.map((opt) => <option value={opt}>{opt}</option>)}
-                        </select>
-              }))
+              this.state.selectThese.map((sel, index) => {
+                return  <div><select name={index} key={index} onChange={this.handleSelectChange}>
+                          <option value='All'>All</option>
+                          {columns && columns.map((val,i) => <option value={val} key={i}>{val}</option>)}
+                        </select></div>
+              })
             }
-            </div>
+          </div>
             <div className="form-group">
               <button type="button" className="btn btn-primary" onClick={this.addSelect}>+</button>
+          </div>
+          <div className="form-group">
+            <label>Where</label>
+            {
+              this.state.whereThese.map((sel) => {
+                return  <div>
+                          <select>
+                            <option value='All'>None</option>
+                            {columns && columns.map(v => <option value={v}>{v}</option>)}
+                          </select>
+                          <h4>is</h4>
+                            <select>
+                            {conditionals && conditionals.map(v => <option value={v}>{v}</option>)}
+                            </select>
+                            <input className="form-control" name="testTextInput2"/>
+                        </div>
+              })
+            }
+          </div>
+            <div className="form-group">
+              <button type="button" className="btn btn-primary">+</button>
             </div>
+            <div className="form-group">
+            <label>Order by</label>
+            {
+              <select>
+               { orderType.map(v => <option value={v}>{v}</option>) }
+              </select>
+            }
+          </div>
           <button type="submit" className="btn btn-success">Submit</button>
         </form>
       </div>
@@ -56,7 +100,7 @@ class myForm extends React.Component {
 const mapState = state => {
   return ({
     table: state.users,
-    columns: (state.users ? Object.keys(state.users[0]) : undefinded)
+    columns: (state.users[0] ? Object.keys(state.users[0]) : undefined)
   })
 }
 
