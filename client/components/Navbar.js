dd74e6707dec8525d9219a41fb7d0cf2429f5d2d
dbui0051 <dennisd.bui@gmail.com>
2017-08-17 18:52:13 -0400

    removed old files

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,21 +1,17 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import history from '../history'
-import {logout} from '../store'
 
 class Navbar extends React.Component {
 
   render() {
-
-    const {handleClick, isLoggedIn} = this.props
 
     return (
       <nav className="navbar navbar-default">
         <div className="container">
           <div className="navbar-left">
             <Link to="/home"><h4>HOME</h4></Link>
-            <Link to="/form"><h4>form sample</h4></Link>
+            <Link to={`/form/${this.props.currentDatabase}`}><h4>Form Sample</h4></Link>
             <Link to="/scatter"><h4>scatter</h4></Link>
             <Link to="/bar"><h4>bar</h4></Link>
             <Link to="/table"><h4>table</h4></Link>
@@ -23,18 +19,6 @@ class Navbar extends React.Component {
             <Link to="/line"><h4>line</h4></Link>
             <Link to="/area"><h4>area</h4></Link>
           </div>
-          {
-            isLoggedIn ?
-              <div className="navbar-right">
-                {/* The navbar will show these links after you log in */}
-                <a href="#" onClick={handleClick}><h4>Logout</h4></a>
-              </div> :
-              <div className="navbar-right">
-                {/* The navbar will show these links before you log in */}
-                <Link to="/login"><h4>Login</h4></Link>
-                <Link to="/signup"><h4>Sign Up</h4></Link>
-              </div>
-          }
         </div>
       </nav>
     )
@@ -43,16 +27,12 @@ class Navbar extends React.Component {
 
 const mapState = (state) => {
   return {
-    isLoggedIn: !!state.user.id
+    currentDatabase: state.currentDatabase
   }
 }
 
 const mapDispatch = dispatch => {
   return {
-    handleClick() {
-      dispatch(logout())
-      history.push('/')
-    }
   }
 }
 
