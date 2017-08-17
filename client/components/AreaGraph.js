0d50c86bd76cb227ee218d7e9745054c9028fd63
Jagnoor Grewal <noor.grewal@berkeley.edu>
2017-08-16 23:03:36 -0700

    area graph working with backend

--- client/components/AreaGraph.js
+++ client/components/AreaGraph.js
@@ -1,69 +1,59 @@
+import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts'
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {fetchUsers} from '../store'
-import {AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip, Legend} from 'recharts'
+import {fetchQueryTable} from '../store'
 
 class AreaGraph extends Component {
 
-  componentDidMount() {
-    this.props.fetchAllUsers()
+  componentDidMount () {
+    const queryInfo = {
+      database: this.props.database || 'capstone1706', 
+      selectThese: this.props.selectThese || ['name', 'age'], 
+      whereThese: this.props.whereThese || [], 
+      table: this.props.table || 'users' 
+    }
+    this.props.fetchQueriedData(queryInfo)
   }
 
-  render() {
-  const {margins, width, height, lineType, dataKey, stroke, data, xdataKey, ydataKey, fill} = this.props
-
-  return (
-    <div className="container">
-      <div className="lineChart">
-      <AreaChart
-        margins={margins}
-        width={width}
-        height={height}
-        data={data}
-      >
-      <Area
-        type={lineType}
-        dataKey={dataKey}
-        stroke={stroke}
-        activeDot={{ stroke: 'blue', strokeWidth: 2, r: 5 }}
-        fill={fill}
-      />
-      <CartesianGrid strokeDasharray="10 10" />
-      <XAxis dataKey={xdataKey} />
-      <YAxis dataKey={ydataKey} />
-      <Tooltip />
-      <Legend />
+  render () {
+    const {queriedTable, width, height, title, x, y, orderBy, whereThese} = this.props
+    const graphData = queriedTable.map((row, index) => {
+      return {name: row[x].slice(0, 4), y: row[y], sales: row['sales']}
+    })
+    return (
+      <AreaChart width={width} height={height} data={graphData}
+            margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+        <XAxis dataKey="name"/>
+        <YAxis/>
+        <CartesianGrid strokeDasharray="3 3"/>
+        <Tooltip/>
+        <Area type='monotone' dataKey='y' stroke='#8884d8' fill='#8884d8' />
       </AreaChart>
-      </div>
-      </div>
-  )
+    )
   }
 }
 
 const mapState = (state, ownProps) => {
   return ({
-      width: 700,
-      height: 300,
-      margins: {
-        left: 100, right: 100, top: 50, bottom: 50
-      },
-      title: 'User sample',
-      stroke: '#ffc658',
-      fill: '#ffc658',
-      lineType: 'monotone',
-      dataKey: 'age',
-      xdataKey: 'id',
-      ydataKey: 'age',
-      data: state.users
-    })
+    title: ownProps.Title || 'Name vs age ',
+    width: ownProps.width || 900,
+    height: ownProps.height || 500,
+    x: ownProps.xAxis|| 'name',
+    y: ownProps.yAxis || 'age', 
+    orderBy: ownProps.orderedBy, 
+    whereThese: ownProps.whereThese, 
+    table: ownProps.table,
+    database: ownProps.database,
+    queriedTable: state.queriedTable
+  })
 }
 
-const mapDispatch = dispatch => {
-  return {
-    fetchAllUsers() {
-      dispatch(fetchUsers())
+const mapDispatch = (dispatch) => {
+  return({
+    fetchQueriedData(queryInfo) {
+      dispatch(fetchQueryTable(queryInfo))
     }
-  }
+  })
 }
 
 export default connect(mapState, mapDispatch)(AreaGraph)
