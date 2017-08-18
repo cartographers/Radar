48e702cb854a2fe28807ecbc8abd7ab1f49300b3
dbui0051 <dennisd.bui@gmail.com>
2017-08-18 01:06:33 -0400

    chore(proper spacing)

--- utils/graphUtility.js
+++ utils/graphUtility.js
@@ -1,28 +1,25 @@
 import {AreaGraph, BarGraph, LineGraph, PieGraph, Scatter, Table} from '../client/components'
-import React, {Component} from 'react'
+import React from 'react'
 
 export const newGraphMaker = (settings) => {
-		const makeGraph = settings.choosenChart
-		const title = settings.Title
-		const width = +settings.width
-		const height = +settings.height
-		const selectThese = settings.selectThese
-		const whereThese = settings.whereThese
-		const orderedBy = settings.orderedBy
-		const database = settings.currentDatabase
-		const table = settings.currentTable
-		console.log('HII NOOOOOR',settings, makeGraph)
-		if(makeGraph === 'Pie'){
-			return <PieGraph title={title} width={width} height={height} selectThese={selectThese} orderedBy={orderedBy} database={database} table={table} whereThese={whereThese}/>
-		}
-		const x = settings.xAxis
-		const y = settings.yAxis
-		if(makeGraph ==='Area') 
-			return <AreaGraph title={title} width={width} height={height} selectThese={selectThese} orderedBy={orderedBy} database={database} table={table} x={x} y={y} whereThese={whereThese} />
-		if(makeGraph ==='Line') 
-			return <LineGraph title={title} width={width} height={height} selectThese={selectThese} orderedBy={orderedBy} database={database} table={table} x={x} y={y} whereThese={whereThese} />
-		if(makeGraph ==='Bar') 
-			return <BarGraph title={title} width={width} height={height} selectThese={selectThese} orderedBy={orderedBy} database={database} table={table} x={x} y={y} whereThese={whereThese} />
-		if(makeGraph ==='Scatter') 
-			return <Scatter title={title} width={width} height={height} selectThese={selectThese} orderedBy={orderedBy} database={database} table={table} x={x} y={y} whereThese={whereThese} />
+    const makeGraph = settings.choosenChart
+    const title = settings.Title
+    const width = +settings.width
+    const height = +settings.height
+    const selectThese = settings.selectThese
+    const whereThese = settings.whereThese
+    const orderedBy = settings.orderedBy
+    const database = settings.currentDatabase
+    const table = settings.currentTable
+    const x = settings.xAxis
+    const y = settings.yAxis
+
+    if (makeGraph === 'Pie'){
+      return <PieGraph title={title} width={width} height={height} selectThese={selectThese} orderedBy={orderedBy} database={database} table={table} whereThese={whereThese}/>
+    }
+    
+    if (makeGraph === 'Area') return <AreaGraph title={title} width={width} height={height} selectThese={selectThese} orderedBy={orderedBy} database={database} table={table} x={x} y={y} whereThese={whereThese} />
+    if (makeGraph === 'Line') return <LineGraph title={title} width={width} height={height} selectThese={selectThese} orderedBy={orderedBy} database={database} table={table} x={x} y={y} whereThese={whereThese} />
+    if (makeGraph ===' Bar') return <BarGraph title={title} width={width} height={height} selectThese={selectThese} orderedBy={orderedBy} database={database} table={table} x={x} y={y} whereThese={whereThese} />
+    if (makeGraph === 'Scatter') return <Scatter title={title} width={width} height={height} selectThese={selectThese} orderedBy={orderedBy} database={database} table={table} x={x} y={y} whereThese={whereThese} />
 }
