eb7699310f7e53bc309ac46f0e16261575cc5d71
Min Hwangbo <min.hwangbo@gmail.com>
2017-08-21 18:51:09 -0400

    fix: change method of getting user file path

--- utils/saveFile.js
+++ utils/saveFile.js
@@ -1,8 +1,11 @@
 const {dialog} = require('electron').remote
 const fs = require('fs')
+const remote = require('electron').remote
+const app = remote.app
+
+const userPath = app.getPath('documents')
 
 function saveFile() {
-
   dialog.showSaveDialog({
     filters: [
       { name: 'text', extensions: ['txt'] }
@@ -16,11 +19,11 @@ function saveFile() {
 }
 
 function saveSettings(settings) {
-  const filePath = process.env.HOME + '/Documents/capstoneSettings/settings.txt'
-  const filedir = process.env.HOME + '/Documents/capstoneSettings/'
-  fs.open(filedir, 'r', (err) => {
+  const filePath = userPath + '/capstoneSettings/settings.txt'
+  const fileDir = userPath + '/capstoneSettings/'
+  fs.open(fileDir, 'r', (err) => {
     if (err) {
-      fs.mkdirSync(filedir, (error) => {
+      fs.mkdirSync(fileDir, (error) => {
         if (error) throw (error)
       })
     }
@@ -31,7 +34,7 @@ function saveSettings(settings) {
 }
 
 function openSettings() {
-  const path = process.env.HOME + '/Documents/capstoneSettings/settings.txt'
+  const path = userPath + '/capstoneSettings/settings.txt'
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) reject(data)
