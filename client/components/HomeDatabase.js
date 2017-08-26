a002ed2bdb4799306f282b2dddf89670aa7cda31
dbui0051 <dennisd.bui@gmail.com>
2017-08-26 03:21:21 -0400

    fix(modal)

--- client/components/HomeDatabase.js
+++ client/components/HomeDatabase.js
@@ -30,14 +30,22 @@ class HomeDatabase extends Component {
     const {databases} = this.props
     const listDatabases = () => {
       return (
-        <Modal.Dialog>
-        <Modal.Body>
-          {databases && databases.map(database => {
-            return (
+        <Modal.Dialog bsSize="large" className=".scrolling.undetached.dimmable.dimmed modal-database">
+          <Modal.Header>
+            <Modal.Title>Pick a Database</Modal.Title>
+          </Modal.Header>
+
+        {databases.map((database, index) => {
+          return (
+            <Modal.Body key={index}>
               <Link to={`/form/${database.datname}`} className="links">{database.datname}</Link>
-            )
-          })}
-        </Modal.Body>
+            </Modal.Body>
+          )
+        })}
+        <Modal.Footer>
+          <Button>Close</Button>
+          <Button bsStyle="primary">Save changes</Button>
+        </Modal.Footer>
       </Modal.Dialog>
       )
     }
@@ -50,19 +58,9 @@ class HomeDatabase extends Component {
             <p className="lead">Postgres thangs and chicken wangs</p>
             <div className="lead">
               <div onClick={this.showDatabase} className="btn btn-lg btn-default">Get Started</div>
-              <Modal.Dialog bsSize="large" className="modal-database">
-              <Modal.Header>
-                <Modal.Title>Pick a Database</Modal.Title>
-              </Modal.Header>
 
               {this.state.renderDatabase ? listDatabases() : null}
 
-              <Modal.Footer>
-                <Button>Close</Button>
-                <Button bsStyle="primary">Save changes</Button>
-              </Modal.Footer>
-            </Modal.Dialog>
-              
             </div>
           </div>
 
